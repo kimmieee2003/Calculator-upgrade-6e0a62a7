c6690df9b6a0f386b5116c7487b92523478f8ee4
Kimberly Hogeveen <2103405@talnet.nl>
2020-03-18 11:32:04 +0100

    verbeter2

--- calculator.php
+++ calculator.php
@@ -6,12 +6,12 @@ echo ("Eerste getal?");
 $Eerste = readline();
 echo ("Tweede getal?");
 $Tweede = readline();
-    if (is_numeric($Eerste)&&is_numeric ($Tweede)) {
+    if(is_numeric($Eerste)&&is_numeric ($Tweede)) {
     echo (int) ($Eerste + $Tweede);
     }else{
     echo"Dit is geen getal";
     }
-    }elseif($uitvoering == "-") {
+}elseif($uitvoering == "-"){
     echo ("Eerste getal?");
     $Derde = readline();
     echo ("Tweede getal?");
@@ -21,7 +21,7 @@ $Tweede = readline();
     }else{
         echo"Dit is geen getal";
     }
-}elseif($uitvoering == "%") {
+}elseif($uitvoering == "%"){
     echo ("Eerste getal?");
     $Vijfde = readline();
     echo ("Tweede getal?");
