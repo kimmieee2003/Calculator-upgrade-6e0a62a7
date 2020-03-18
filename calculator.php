a07e4f5e7f4b5178e4de897eb7cea2a64fb91fd4
Kimberly Hogeveen <2103405@talnet.nl>
2020-03-18 11:13:52 +0100

    Uitwerking van de exercise

---
+++ calculator.php
@@ -0,0 +1,39 @@
+<?php 
+echo ("Welke operatie wil je uitvoeren? (+, -, %)");
+$uitvoering = readline();
+if ($uitvoering == "+") {
+echo ("Eerste getal?");
+$Eerste = readline();
+echo ("Tweede getal?");
+$Tweede = readline();
+if(is_numeric($Eerste)&&is_numeric($Tweede)){
+    echo (int) ($Eerste + $Tweede);
+}else{
+    echo"Dit is geen getal";
+}
+}elseif($uitvoering == "-") {
+    echo ("Eerste getal?");
+    $Derde = readline();
+    echo ("Tweede getal?");
+    $Vierde = readline();
+    if(is_numeric($Derde)&&is_numeric($Vierde)){
+        echo (int) ($Derde - $Vierde);
+    }else{
+        echo"Dit is geen getal";
+    }
+}elseif($uitvoering == "%") {
+    echo ("Eerste getal?");
+    $Vijfde = readline();
+    echo ("Tweede getal?");
+    $Zesde = readline();
+    if(is_numeric($Vijfde)&&is_numeric($Zesde)){
+        echo (int) ($Vijfde % $Zesde);
+    }else{
+        echo"Dit is geen getal";
+    }
+}else{
+    echo "Dit is geen operatie.";
+}
+?>
+
+?>
